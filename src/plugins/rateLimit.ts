c44c867a9043398e20abf090c0f18d06d3601bfc
MyNameIsNeXTSTEP <madmath@bk.ru>
2025-10-31 15:56:59 +0300

    Plugins: Rate limiter by Fastify core plugins

---
+++ src/plugins/rateLimit.ts
@@ -0,0 +1,8 @@
+import fp from 'fastify-plugin';
+import rateLimit from '@fastify/rate-limit';
+
+export default fp(async (fastify) => {
+  await fastify.register(rateLimit, {
+    // ...to be defined later
+  });
+});
